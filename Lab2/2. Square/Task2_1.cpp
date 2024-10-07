eb43abff94993330f06abec73d6e61a8fad43ba2
av-hart <av.hart.237@gmail.com>
2024-10-07 20:22:05 +0300

    Create Task2_1.cpp

---
+++ Lab2/2. Square/Task2_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "A--------B              xxxxxxxxxx" << endl;
+    cout << "|        |              xxxxxxxxxx" << endl;
+    cout << "|        |      or      xxxxxxxxxx" << endl;
+    cout << "|        |              xxxxxxxxxx" << endl;
+    cout << "D--------C              xxxxxxxxxx" << endl;
+
+    cout << "\n@Ann Hart" << endl;
+
+    cin.ignore();
+    cin.get();
+    return 0;
+}
