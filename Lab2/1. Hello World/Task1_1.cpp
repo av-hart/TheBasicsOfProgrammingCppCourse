3e596787b0f129d55c93257657fe3af78688757f
av-hart <av.hart.237@gmail.com>
2024-10-07 20:19:17 +0300

    Create Task1_1.cpp

---
+++ Lab2/1. Hello World/Task1_1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello, world!" << endl;
+    cout << "\n@Ann Hart" << endl;
+
+    cin.ignore();
+    cin.get();
+    return 0;
+}
