1ba86a4b8eb40f9c4d3df493c1ff6a69d8a10e1e
av-hart <av.hart.237@gmail.com>
2024-10-18 23:16:11 +0300

    Create Task1.cpp

---
+++ Lab3/Task1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+    cout << "What is your name? ";
+    cin >> name;
+
+    cout << "Hello, " << name << "!" << endl;
+    
+    cout << "\n@Ann Hart" << endl;
+
+    return 0;
+}
